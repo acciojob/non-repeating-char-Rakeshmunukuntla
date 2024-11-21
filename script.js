538d523be72382a868b7c1b588c5f720296c79cb
Rakesh Munukuntla <130857014+Rakeshmunukuntla@users.noreply.github.com>
2024-11-21 10:25:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
-function firstNonRepeatedChar(str) {
+function firstNonRepeatedChar(str) 
+{
  // Write your code here
+	
+	for (let index = 0; index < bound; index++) 
+	{
+		let c=0;
+		for (let j = 0; j < bound; j++) 
+		{
+			if (index==j) 
+			{
+				continue;
+			}
+			else
+			{
+				if(str[index]==str[j])
+				{
+					c++;
+				}
+			}
+			if(c==0)
+			{
+				return str[index];
+			}
+		}
+	}
+	return null;
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+//const input = prompt("Enter a string");
+//alert(firstNonRepeatedChar(input)); 
+
